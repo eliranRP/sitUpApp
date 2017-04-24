1fe2a70751d429e5a550ceb1180a6e73676c5836
eliranRP <eliranazulay@gmail.com>
2017-04-24 13:24:25 +0300

    final

---
+++ platforms/browser/www/js/controllers/events.js
@@ -0,0 +1,34 @@
+﻿mainApp.controller("HomeController", ["$scope", "$http", '$rootScope', '$firebaseAuth','$firebaseArray',
+function ($scope, $http, $rootScope, $firebaseAuth, $firebaseArray) {
+    $scope.title = "ארועים";
+    $scope.games = [];
+    $scope.logos = [];
+    var ref = firebase.database().ref();
+    var auth = $firebaseAuth();
+    auth.$onAuthStateChanged(function (authUser) {
+        if (authUser) {
+
+            //load data
+            var eventsRef = ref.child('events'); // all events
+            var events = $firebaseArray(eventsRef);
+            events.$loaded().then(function (data) {
+                console.log(data)
+                $rootScope.events = events;
+            }); //make sure data is loaded
+
+
+
+
+
+            $scope.redirect = function (url) {
+                mainView.router.loadPage(url)
+            }
+
+            $scope.go = function (url, event) {
+                $rootScope.currentEvent = event;
+                mainView.router.loadPage(url);
+            };
+        }
+    });
+
+}])
---
+++ platforms/browser/www/js/controllers/registration.js
@@ -0,0 +1,33 @@
+﻿mainApp.controller('RegistrationController',
+  ['$scope', 'Authentication',
+  function ($scope, Authentication) {
+
+      $scope.login = function () {
+          Authentication.login($scope.user);
+          console.log('Im login from registration')
+      }; //login
+
+      $scope.googleLogin = function () {
+          Authentication.googleLogin();
+      }, //google Login
+
+      $scope.FBLogin = function () {
+          Authentication.FBLogin();
+      }, //FaceBook Login
+
+
+      $scope.logout = function () {
+          Authentication.logout();
+          mainView.router.loadPage("login.html")
+      }; //logout
+
+      $scope.register = function () {
+          Authentication.register($scope.user);
+      }; //register
+
+      $scope.resetPassword = function () {
+          Authentication.resetPassword($scope.resetEmail);
+      }; //register
+
+  }]); //Controllerr
+
